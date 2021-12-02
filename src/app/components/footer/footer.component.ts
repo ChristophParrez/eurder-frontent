5a2057eaaabfa449a89aca41ab17819516af3961
Christoph <christoph.parrez@gmail.com>
2021-12-02 13:38:17 +0100

    Basic Layout

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  year: number = new Date().getFullYear();
+
+  constructor() {}
 
   ngOnInit(): void {
   }
